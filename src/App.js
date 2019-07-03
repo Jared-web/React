a1953230f5a9411ac474bfbeab07b09f9878c908
Jared Aitken <51947929+Jared-web@users.noreply.github.com>
2019-07-03 16:08:17 +0200

    Add files via upload
    Changed the UI, added the testing environment.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './img/data.svg';
 import './App.css';
+import Testing from './img/Testing.png';
+import Production from './img/Production.png';
+import ServerOnline from './img/legend-icons/green-dot.png';
+import ServerDown from './img/legend-icons/red-dot.png';
+import ServerOther from './img/legend-icons/black-dot.png';
+import serverlist from './servers/server_list';
 
 class App extends Component {
   render() {
@@ -13,28 +19,50 @@ class App extends Component {
 
   <div className="Key">
     <br></br>
-    <div>Green = Up </div>
+    <div> <img src={ServerOnline} alt="" className="ServerOnline" /> Online </div>
     <br></br>
-    <div>Red = Failed </div>
+    <div> <img src={ServerDown} alt="" className="ServerDown" /> Down </div>
     <br></br>
-    <div> Grey = Down</div>
+    <div> <img src={ServerOther} alt="" className="ServerOther" /> Other </div>
     <br></br>
   </div>
 
   <div className="Prod-style">
-    <h2 className="h2">  Production </h2>
+    <h2 className="h2"> Production </h2>
     <div className="display-production-server">
       <div className="row">
 
-        <div className="Server-Block" onClick={(e) => {this.onClick()} }> Prod1 </div>
-        <div className="Server-Block" onClick={(e) => {this.onClick()} }> Prod2 </div>
-        <div className="Server-Block" onClick={(e) => {this.onClick()} }> Prod3 </div>
+        <div className="Server-Block" onClick={(e) => {this.onClick()} }>
+        <img className="server-img" src={Production} alt="Not found."/> Prod 1 </div>
+        <div className="Server-Block" onClick={(e) => {this.onClick()} }>
+        <img className="server-img" src={Production} alt="Not found."/> Prod 2 </div>
+        <div className="Server-Block" onClick={(e) => {this.onClick()} }>
+        <img className="server-img" src={Production} alt="Not found."/> Prod 3 </div>
+
+      </div>
+    </div>
+  </div>
+
+<br></br>
+
+  <div className="Testing-style">
+    <h2 className="h2"> Testing </h2>
+    <div className="display-testing-server">
+      <div className="row">
+
+        <div className="Server-Block" onClick={(e) => {this.onClick()} }>
+        <img className="server-img" src={Testing} alt="Not found."/> Test 1 </div>
+        <div className="Server-Block" onClick={(e) => {this.onClick()} }>
+        <img className="server-img" src={Testing} alt="Not found."/> Test 2 </div>
+        <div className="Server-Block" onClick={(e) => {this.onClick()} }>
+        <img className="server-img" src={Testing} alt="Not found."/> Test 3 </div>
 
       </div>
     </div>
   </div>
 
 </div>
+
 
 
       );
--- src/servers/server_list.js
+++ src/servers/server_list.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const list =['https://cognition.dev.stackworx.cloud/api/status', 'https://ord.dev.stackworx.io/health'];
 
-server1 - https://cognition.dev.stackworx.cloud/api/status
-server2 - https://ord.dev.stackworx.io/health
-server3 - https://api.durf.dev.stackworx.io/health
-server4 - https://prima.run/health
-server5 - https://stackworx.io/
-server6 - https://stackworx.io/
+const serverlist = () => (
+  <ul>
+    {list.map(function(item) {
+      return <li key={item}>{item}</li>;
+    })}
+  </ul>
+);
+
+//Prod1 - https://cognition.dev.stackworx.cloud/api/status
+//Prod2 - https://ord.dev.stackworx.io/health
+//Prod3 - https://api.durf.dev.stackworx.io/health
+//Test4 - https://prima.run/health
+//Test5 - https://stackworx.io/
+//Test6 - https://stackworx.io/
+
+export default serverlist
